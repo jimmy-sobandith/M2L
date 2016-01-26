14848e2bcbaa33bb642e5c5c17834268fa3536ab
jimmy sobandith <jimmy sobandith>
2016-01-26 17:09:29 +0100

    Modification de l'auteur

--- models/menu.py
+++ models/menu.py
@@ -11,7 +11,7 @@ response.title = "M2L"
 response.subtitle = 'Maison des Ligues de Lorraine'
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'Maelle Taurand'
+response.meta.author = 'Jimmy Sobandith'
 response.meta.description = 'Portail de la maison des Ligues de Lorraine'
 response.meta.keywords = 'Ligues, Lorraine, Sport'
 response.meta.generator = 'Portail de la maison des Ligues de Lorraine'
